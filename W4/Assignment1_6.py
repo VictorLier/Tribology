1654e178155524bf919647a0f2ab5be4c1314cae
VictorLier <victor@lier.dk>
2024-10-23 01:59:50 +0200

    Updated to find same load

--- W4/Assignment1_6.py
+++ W4/Assignment1_6.py
@@ -5,7 +5,7 @@ from P3 import finite
 
 
 class AS6:
-    def __init__(self, number_pads: int = 20, type: int = 0, notch_size: float = 0.1, outer_radius: float = 0.250, inner_radius: float = 0.095 , max_load: float = 10e5, kinematic_viscosity: float = 32e-6, minimum_height: float = 60e-6, density: float = 860, specific_heat: float = 2000, running_speed: float = 1000) -> None:
+    def __init__(self, number_pads: int = 20, type: int = 0, notch_size: float = 0.1, outer_radius: float = 0.250/2, inner_radius: float = 0.095 , max_load: float = 4e5, kinematic_viscosity: float = 32e-6, minimum_height: float = 60e-6, density: float = 860, specific_heat: float = 2000, running_speed: float = 1000) -> None:
         '''
         number_pads (int): number of pads of the bearing
 
@@ -38,24 +38,25 @@ class AS6:
         self.notch_size = notch_size
         self.r_o = outer_radius
         self.r_i = inner_radius
-        self.r_m = (self.r_o + self.r_i) / 2
+        self.r_m = (self.r_o + self.r_i) / 2    # The mean radius of the bearing
+        self.b = self.r_o - self.r_i            # The width of the bearing
         self.max_load = max_load
         self.nu = kinematic_viscosity
         self.h_0 = minimum_height
         self.rho_0 = density
-        self.eta_0 = self.nu * self.rho_0
+        self.eta_0 = self.nu * self.rho_0       # The dynamic viscosity
         self.Cp = specific_heat
         self.rpm = running_speed
         self.calc_length()
         self.calc_velocity()
+        self.load_per_unit_width()
 
 
     def calc_length(self) -> None:
         '''
         Calculates the length of the pad
         '''
-        pad_angle = 360 / self.number_pads * (1 - self.notch_size) # The angle of the pad
-        self.l = 2 * np.pi * self.r_m * pad_angle / 360 # The length of the pad
+        self.l = 2*np.pi * self.r_m/self.number_pads * (1 - self.notch_size) # The length of the pad
 
 
     def calc_velocity(self) -> None:
@@ -66,6 +67,13 @@ class AS6:
         self.u_b = 2 * np.pi * self.r_m * self.rpm / 60
 
 
+    def load_per_unit_width(self) -> None:
+        '''
+        Calculates the load per unit width
+        '''
+        self.wm_z = self.max_load / self.number_pads / self.b # (8.1)
+
+
     def geometry_parameters(self, plot: bool = False, print_bol: bool = False):
         '''
         Prints and calcualets the expressions the "optimal" geometry parameters of the bearing
@@ -73,13 +81,14 @@ class AS6:
         plot (bool): If True, plots the shoulder height
         '''
         if self.type == 0: # Fixed inclined pads
-            if print_bol:
-                print("Fixed inclined pads: h = h_0 + s_h * (1 - x/l) with the optimal being: s_h = sqrt(2) * h_0") # (8.16) (8.28)
-            x = np.linspace(0, self.l, 100)
-            self.s_h = np.exp(2) * self.h_0
-            self.H_0 = self.h_0 / self.s_h
-            self.P_m = 3 / (2* self.H_0 * (1 + self.H_0) * (1 + 2 * self.H_0)) # (8.26)
+            x = np.linspace(0, self.l, 2)
+            self.H_0 = np.sqrt(2)/2 # 8.28
+            self.W_z = 6*np.log((self.H_0 + 1)/self.H_0) - 12/(1+2*self.H_0) #8.30
+            self.s_h = np.sqrt(self.wm_z * self.W_z * self.eta_0 * self.u_b) * self.l / self.wm_z
+            self.h_0 = self.s_h * self.H_0
             h = self.h_0 + self.s_h * (1 - x/self.l)
+            if print_bol:
+                print(f"Fixed inclined pads: The optimal being: s_h = {self.s_h:.3g} and h_0 = {self.h_0:.3g}")
             if plot:
                 plt.figure()
                 plt.plot(x, h)
@@ -89,15 +98,19 @@ class AS6:
                 plt.title('Shoulder height - Fixed inclined pads')
                 plt.show()
 
+                np.savetxt('W4/data/fixedProfile.txt', np.array([x, h]).T)
+
         if self.type == 1: # Parallel-step
-            if print_bol:
-                print("Parallel-step: H_0 = h_0 / s_h = 1.155 and n_s = 0.7182") # (8.68) (8.69)
-            self.s_h = self.h_0 / 1.155
-            self.H_0 = 1.155
             self.n_s = 0.7182
-            self.P_m = 6*self.n_s / ((1-self.n_s) * (self.H_0 + 1)**3 + self.n_s * self.H_0**3) # (8.65)
-            x = np.array([0, self.n_s*self.l, self.n_s*self.l, self.l])
+            self.H_0 = 1.155
+            self.W_z = 3 * self.n_s * (1 - self.n_s) / ( (1 - self.n_s) * (self.H_0 + 1)**3 + self.n_s * self.H_0**3)
+            self.s_h = np.sqrt(self.wm_z*self.W_z*self.eta_0*self.u_b)*self.l / self.wm_z
+            self.h_0 = self.s_h * self.H_0
+
+            x = np.array([0, self.l*self.n_s, self.l*self.n_s, self.l])
             h = np.array([self.h_0+self.s_h, self.h_0+self.s_h, self.h_0, self.h_0])
+            if print_bol:
+                print(f"Parallel-step: h_0 = {self.h_0:.3g} and s_h = {self.s_h:.3g}")
             if plot:
                 plt.figure()
                 plt.plot(x, h)
@@ -107,6 +120,7 @@ class AS6:
                 plt.title('Shoulder height - Parallel-step')
                 plt.show()
 
+                np.savetxt('W4/data/parallelProfile.txt', np.array([x, h]).T)
 
     def friction(self):
         '''
@@ -118,6 +132,7 @@ class AS6:
             self.mu = up/ned # (8.34)
         
         if self.type == 1:
+            self.P_m = 6 * self.n_s * (1 - self.n_s) / ( (1 - self.n_s) * (self.H_0 + 1)**3 + self.n_s * self.H_0**3) # (8.65
             self.mu = self.s_h / self.l * (1 + (2 * (self.H_0 + 1 - self.n_s)) / (self.P_m * self.H_0*(1+self.H_0))) # (8.80)
 
 
@@ -178,7 +193,7 @@ if __name__ == '__main__':
         par.geometry_parameters(plot = True, print_bol = True)
 
 
-    if False: # Part b
+    if True: # Part b
         print("Part b:")
         inc = AS6(type = 0)
         inc.geometry_parameters()
@@ -189,12 +204,12 @@ if __name__ == '__main__':
         par.friction()
 
         if inc.mu < par.mu:
-            print(f"The friction coefficient for the fixed inclined pads is lower than the parallel-step: {inc.mu:.4f} < {par.mu:.4f}")
+            print(f"The friction coefficient for the fixed inclined pads is lower than the parallel-step: {inc.mu:.4g} < {par.mu:.4g}")
         else:
-            print(f"The friction coefficient for the parallel-step is lower than the fixed inclined pads: {par.mu:.4f} < {inc.mu:.4f}")
+            print(f"The friction coefficient for the parallel-step is lower than the fixed inclined pads: {par.mu:.4g} < {inc.mu:.4g}")
 
 
-    if True: # Part c
+    if False: # Part c
         print("Part c:")
         inc = AS6(type = 0)
         inc.geometry_parameters()
